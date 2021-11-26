283920be361c9d2017405b58e17e535135acb1df
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2021-11-26 22:29:24 +0200

    init project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '87645e84ce67d8738597b864a042ae11'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '719c656c-0642-4ae7-b5ea-d9be0fc77d3e'
+};
